9c084c0a0b4ed45b29c3b109b4e6de938e2b3a31
jzapata <jzapata@lifefile.net>
2023-05-06 00:37:52 -0500

    feat: add middlewares for auth and guest users

--- pages/me.vue
+++ pages/me.vue
@@ -1,9 +1,12 @@
-<script setup>
+<script setup lang="ts">
 import axios from "axios";
- const {data: user} = await axios.get("/user")
+definePageMeta({
+    middleware: ["auth"],
+});
+const {data: user} = await axios.get("/user")
 </script>
 
 <template>
-  <p><strong>User Name:</strong> {{ user.name }}</p>
-  <p><strong>User Email:</strong> {{  user.email }}</p>
+    <p><strong>User Name:</strong> {{ user.name }}</p>
+    <p><strong>User Email:</strong> {{ user.email }}</p>
 </template>
