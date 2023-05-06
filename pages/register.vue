d1ebedc4fb06216d2f09351eda62b176cc8b489e
jzapata <jzapata@lifefile.net>
2023-05-05 23:22:34 -0500

    improve: register.vue using short url

--- pages/register.vue
+++ pages/register.vue
@@ -20,7 +20,7 @@ const form = ref<RegisterPayload>({
 });
 
 async function register(payload: RegisterPayload) {
-    const res = await axios.post("/api/register", payload);
+    const res = await axios.post("/register", payload);
     console.log(res);
 }
 
