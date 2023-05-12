e1a175c786fc8c4aca6873b4c0a98800973e8b98
jzapata <jzapata@lifefile.net>
2023-05-12 04:33:36 -0500

    add new interfaces

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,28 +1,5 @@
 import axios from "axios";
-
-export interface LoginPayload {
-    email: string;
-    password: string;
-}
-
-export interface RegisterPayload {
-    name: string;
-    email: string;
-    password: string;
-    password_confirmation: string;
-}
-
-interface User {
-    email: string;
-    email_verified_at?: Date;
-    id: number;
-    name: string;
-    two_factor_confirmed_at?: Date;
-    two_factor_recovery_codes?: number;
-    two_factor_secret?: string;
-    updated_at: Date;
-    created_at: Date;
-}
+import {User, RegisterPayload, LoginPayload} from "~/types/types"
 
 const user = ref<User | null>(null);
 
--- middleware/guest.ts
+++ middleware/guest.ts
@@ -1,5 +1,3 @@
-import axios, {AxiosError} from "axios";
-
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const {user, initUser} = useAuth();
     await initUser();
